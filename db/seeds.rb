fb56de461b0e661c6170269cde64fb9186873d0f
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2011-09-29 00:51:12 +0200

    Add db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+event = Event.create(planned_at: Time.zone.now)
+3.times do
+  Presentation.create(topic: "Lorem ipsum dolor sit amet", event: event)
+end
