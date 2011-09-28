816c08be259600d93b108a38991c3b9a6de46fb3
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2011-09-28 13:37:48 +0200

    Set up rspec, spork, livereload, capyabra and foreman

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'spork'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'capybara/rspec'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.use_transactional_fixtures = true
+  end
+end
+
+Spork.each_run do
+end
