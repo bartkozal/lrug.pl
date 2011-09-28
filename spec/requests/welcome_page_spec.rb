1cfff34468984039f180cc1e388c7b48803e3fa6
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2011-09-28 15:14:46 +0200

    Add sample welcome page

--- spec/requests/welcome_page_spec.rb
+++ spec/requests/welcome_page_spec.rb
@@ -3,11 +3,12 @@
 require "spec_helper"
 
 feature "Welcome page" do
-  scenario "Visit www.lrug.pl" do
+  scenario "visit /" do
+    visit '/'
     page.should have_css('header h1', text: 'Łódź Ruby User Group')
     page.should have_css('ul#presentations')
-    page.should have_css('#address', text: 'Łąkowa 11, biuro Ragnarson')
-    page.should have_css('#date', text: '20 października 2011, 16:30 +')
-    page.should have_css('footer p', text: 'archiwum | kod źródłowy')
+    page.should have_css('#address p', text: 'Łąkowa 11, biuro Ragnarson')
+    page.should have_css('#date p', text: '20 października 2011, 16:30+')
+    page.should have_css('footer p', text: 'archiwum lrug | kod źródłowy')
   end
 end
