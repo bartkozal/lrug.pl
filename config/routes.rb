8f3076f55cbeb05035e6a7d4012716710e87d43e
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2011-09-28 13:46:05 +0200

    Create welcome controller, close #6

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ LrugPl::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#show'
 
   # See how all your routes lay out with "rake routes"
 
