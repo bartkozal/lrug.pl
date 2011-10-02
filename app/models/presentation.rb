8e2b26019cb83ffffdee559d6a5ea00a82c7c649
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2011-10-03 01:16:28 +0200

    Add validation for presentation topic

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -2,6 +2,8 @@ class Presentation < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
 
+  validates_presence_of :topic, :on => :update
+
   def to_s
     topic
   end
--- spec/models/presentation_spec.rb
+++ spec/models/presentation_spec.rb
@@ -4,4 +4,14 @@ describe Presentation do
   it "returns topic" do
     build(:presentation).to_s.should == "Lorem ipsum dolor sit amet"
   end
+
+  it "success validation with blank topic on create" do
+    build(:presentation, topic: nil).should have(0).errors_on(:topic)
+  end
+
+  it "fails validation with blank topic on update" do
+    presentation = create(:presentation, topic: nil)
+    presentation.update_attributes(topic: nil)
+    presentation.should have(1).errors_on(:topic)
+  end
 end
