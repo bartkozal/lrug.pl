82336468aafeb8f79cb6857b1fc334305269213f
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2011-10-02 18:12:45 +0200

    Remove seeds

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-event = Event.create(planned_at: Time.zone.now)
-user = User.create(first_name: "Johnny", last_name: "Cash")
-3.times do
-  Presentation.create(topic: "Lorem ipsum dolor sit amet", event: event, user: user)
-end
