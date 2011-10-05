fd48197e247363097450e2957cdc392f65f39072
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2011-10-05 22:46:52 +0200

    Fix administration_spec

--- spec/requests/administration_spec.rb
+++ spec/requests/administration_spec.rb
@@ -3,9 +3,13 @@
 require 'spec_helper'
 
 feature 'administration panel' do
-  scenario 'list events' do
+  background do
     create(:event)
+    page.driver.header('Authorization', %Q{Basic #{Base64.encode64("admin:example")}})
     visit admin_path
+  end
+
+  scenario 'list events' do
     page.should have_selector('form.new_event')
     within 'ul li' do
       page.should have_content('1 września 2011, 10:00')
@@ -14,7 +18,6 @@ feature 'administration panel' do
   end
 
   scenario 'create event' do
-    visit admin_path
     select '30', from: 'event_planned_at_3i'
     select 'Listopad', from: 'event_planned_at_2i'
     select '2011', from: 'event_planned_at_1i'
@@ -26,15 +29,12 @@ feature 'administration panel' do
   end
 
   scenario 'create event fails' do
-    visit admin_path
     click_button 'zaplanuj'
     page.current_path.should == admin_path
     page.should have_content("Event nie został stworzony")
   end
 
   scenario 'delete event' do
-    create(:event)
-    visit admin_path
     click_link 'usuń'
     page.should_not have_content('1 września 2011, 10:00')
   end
