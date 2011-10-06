56e8246e326dede1f338d7ba069d72247719d8d2
Bartek Kozal <bartlomiejkozal@gmail.com>
2011-10-06 14:11:41 +0200

    Simplify layouts/application spec

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -1,19 +1,14 @@
 require 'spec_helper'
 
 describe 'layouts/application.html.haml' do
-  def current_user(bool)
-    klass = %Q{def current_user; #{bool}; end; helper_method :current_user}
-    controller.singleton_class.class_eval(klass)
-  end
-
   it 'displays logout link for current user' do
-    current_user true
+    view.stub(:current_user) { true }
     render
     rendered.should have_link('Wyloguj', href: logout_path)
   end
 
   it 'displays login with github' do
-    current_user false
+    view.stub(:current_user) { false }
     render
     rendered.should have_link('Zaloguj przez GitHub', href: '/auth/github')
   end
