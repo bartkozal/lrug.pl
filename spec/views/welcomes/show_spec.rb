72b6eb36ca0f437228092887f147701da587eb42
Bartlomiej Kozal <bkzl@me.com>
2012-04-26 08:07:06 +0200

    Update address

--- spec/views/welcomes/show_spec.rb
+++ spec/views/welcomes/show_spec.rb
@@ -8,8 +8,8 @@ describe "welcomes/show" do
     render
 
     rendered.should have_content('1 września 2020, 10:00')
-    rendered.should have_content('Łąkowa 11, biuro Ragnarson, 1. piętro')
-    rendered.should have_content('Budynek główny (ceglany), 1. klatka')
+    rendered.should have_content('Aleja Kościuszki 32/7, 3. piętro')
+    rendered.should have_content('Biuro Ragnarson')
 
     rendered.should have_selector('.map')
     rendered.should have_selector('ol.presentations')
