02b2917366023f12c347808ddb982b4ab51183b8
Bartlomiej Kozal <bkzl@me.com>
2013-07-14 17:29:23 +0200

    Add map

--- build/javascripts/script.js
+++ build/javascripts/script.js
@@ -1,4 +1,24 @@
 (function() {
+  var map, mapSettings, markerSettings, position;
 
+  position = new google.maps.LatLng(51.766067, 19.454545);
+
+  mapSettings = {
+    zoom: 15,
+    center: position,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    streetViewControl: false,
+    mapTypeControl: false
+  };
+
+  map = new google.maps.Map(document.querySelector('#map'), mapSettings);
+
+  markerSettings = {
+    position: position,
+    map: map,
+    flat: true
+  };
+
+  new google.maps.Marker(markerSettings);
 
 }).call(this);
