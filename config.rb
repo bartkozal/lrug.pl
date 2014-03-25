aa43fed24469fa0be672a542300f74a9f84a0b64
Bartlomiej Kozal <bkzl@me.com>
2014-03-25 23:48:24 +0100

    Fix #50 - store data in json

--- config.rb
+++ config.rb
@@ -5,6 +5,16 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+helpers do
+  def link_to_if(condition, caption)
+    condition ? link_to(caption, condition) : caption
+  end
+
+  def current_event
+    data.events.first
+  end
+end
+
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
