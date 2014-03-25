741a601541a6036b80b90191331dc8aa8f9f9d0f
Bartlomiej Kozal <bkzl@me.com>
2014-03-26 00:11:20 +0100

    Optimaize build

--- config.rb
+++ config.rb
@@ -15,6 +15,12 @@ helpers do
   end
 end
 
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+end
+
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
